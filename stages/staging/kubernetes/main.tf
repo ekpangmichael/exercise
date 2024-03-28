1996f3cec39156376c5ef6b8ca42c4b1f2a25e65
Michael Ekpang <career.ekpangmichael@gmail.com>
2024-03-28 16:45:17 +0100

    node groups

--- stages/staging/kubernetes/main.tf
+++ stages/staging/kubernetes/main.tf
@@ -27,13 +27,13 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    example = {
+    group-1 = {
       min_size     = 1
       max_size     = 10
       desired_size = 1
 
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
+      instance_types = ["m7g.medium"]
+      capacity_type  = "ON_DEMAND"
     }
   }
 }
